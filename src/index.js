123b3084aad4579000c5b79fc41f2ea5aad7f0ef
amanjanaagal <amankirmara144@gmail.com>
2024-01-31 14:56:55 +0530

    new code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
-dotenv.config()
-
-
-
-
-
+dotenv.config();
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        `Server is running at : http://locahost:${process.env.PORT || 3000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !! ", err);
+  });
 
 
 
@@ -22,6 +32,9 @@ connectDB()
 
 
 
+
+
+  
 
 // ( async () => {
 //     try {
@@ -37,5 +50,4 @@ connectDB()
 //         console.error("ERROR: ", error)
 //         throw error
 //     }
-// })() 
-
+// })()
